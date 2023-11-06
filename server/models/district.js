10fe9bd91ec97a0744b1b346951cb135667d0e92
Haroon Frotan <haroon_fro@hotmail.com>
2023-11-06 11:19:45 -0500

    districts updated

--- server/models/district.js
+++ server/models/district.js
@@ -1,7 +1,7 @@
 const { Schema } = require('mongoose')
 const districtSchema = new Schema (
     {
-        id: {type: String, required: true},
+       
         name: {type: String, required: true},
         population: {type: Number, required: true},
         fact: {type: String, required: true}
