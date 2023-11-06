78e87c089330994e8002d95e55c8c92936bbc053
annemarielewis <a.m.lewisfilms@gmail.com>
2023-11-06 13:09:47 -0700

    restaurant seed

--- server/seed/restaurants.js
+++ server/seed/restaurants.js
@@ -1,13 +1,35 @@
-const db = require('../db')
-const { Restaurant } = require('../models')
+const db = require("../db");
+const { Restaurant } = require("../models");
 
-db.on('error', console.error.bind(console, 'Mongodb connection error:'))
+db.on("error", console.error.bind(console, "Mongodb connection error:"));
 
 const main = async () => {
-    const restaurantList = [
-        {
-            name: 'Dukes Seafood'
-            district:
-        }
-    ]
-}
+  const Magnolia = await District.find({
+    name: "Magnolia & Queen Anne",
+  });
+  const CentralSeattle = await District.find({
+    name: "Central Seattle",
+  });
+  const DowntownSeattle = await District.find({
+    name: "Downtown Seattle",
+  });
+  const NorthwestSeattle = await District.find({
+    name: "Northwest Seattle",
+  });
+  const NortheastSeattle = await District.find({
+    name: "Northeast Seattle",
+  });
+  const Delridge = await District.find({
+    name: "West Seattle & Delridge",
+  });
+  const Southeast = await District.find({
+    name: "Southeast Seattle",
+  });
+
+  const restaurantList = [
+    {
+      name: "Dukes Seafood",
+      district: Magnolia[0]._id,
+    },
+  ];
+};
