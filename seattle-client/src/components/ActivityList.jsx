985554ffdd7d190104f35afd5154514b8eab7e8f
Rhys Frazier <rhysrfrazier@gmail.com>
2023-11-06 20:48:44 -0500

    reactstrap is my worst enemy

---
+++ seattle-client/src/components/ActivityList.jsx
@@ -0,0 +1,235 @@
+import React from 'react'
+import { Card, CardImg, CardBody, CardTitle, CardSubtitle, CardGroup } from 'reactstrap'
+
+export default function ActivityList() {
+    return (
+        <div className='container'>
+            <CardGroup>
+
+                <Card
+                    style={{
+                        width: '18rem'
+                    }}
+                >
+                    <CardImg
+                        alt='generic decorative image'
+                        src='assets/seahawks.png'
+                        width="100%"
+                    />
+
+                    <CardBody>
+                        <CardTitle tag="h5">
+                            name (e.g. Seahawks v. Someone at Place)
+                        </CardTitle>
+                        <CardSubtitle
+                            className="mb-2 text-muted"
+                            tag="h6"
+                        >
+                            Type here (e.g. 'sporting event')
+                        </CardSubtitle>
+                    </CardBody>
+                </Card>
+
+                <Card
+                    style={{
+                        width: '18rem'
+                    }}
+                >
+                    <CardImg
+                        alt='generic decorative image'
+                        src='assets/seahawks.png'
+                        width="100%"
+                    />
+
+                    <CardBody>
+                        <CardTitle tag="h5">
+                            name (e.g. Seahawks v. Someone at Place)
+                        </CardTitle>
+                        <CardSubtitle
+                            className="mb-2 text-muted"
+                            tag="h6"
+                        >
+                            Type here (e.g. 'sporting event')
+                        </CardSubtitle>
+                    </CardBody>
+                </Card>
+
+                <Card
+                    style={{
+                        width: '18rem'
+                    }}
+                >
+                    <CardImg
+                        alt='generic decorative image'
+                        src='assets/seahawks.png'
+                        width="100%"
+                    />
+
+                    <CardBody>
+                        <CardTitle tag="h5">
+                            name (e.g. Seahawks v. Someone at Place)
+                        </CardTitle>
+                        <CardSubtitle
+                            className="mb-2 text-muted"
+                            tag="h6"
+                        >
+                            Type here (e.g. 'sporting event')
+                        </CardSubtitle>
+                    </CardBody>
+                </Card>
+
+                <Card
+                    style={{
+                        width: '18rem'
+                    }}
+                >
+                    <CardImg
+                        alt='generic decorative image'
+                        src='assets/seahawks.png'
+                        width="100%"
+                    />
+
+                    <CardBody>
+                        <CardTitle tag="h5">
+                            name (e.g. Seahawks v. Someone at Place)
+                        </CardTitle>
+                        <CardSubtitle
+                            className="mb-2 text-muted"
+                            tag="h6"
+                        >
+                            Type here (e.g. 'sporting event')
+                        </CardSubtitle>
+                    </CardBody>
+                </Card>
+
+                <Card
+                    style={{
+                        width: '18rem'
+                    }}
+                >
+                    <CardImg
+                        alt='generic decorative image'
+                        src='assets/seahawks.png'
+                        width="100%"
+                    />
+
+                    <CardBody>
+                        <CardTitle tag="h5">
+                            name (e.g. Seahawks v. Someone at Place)
+                        </CardTitle>
+                        <CardSubtitle
+                            className="mb-2 text-muted"
+                            tag="h6"
+                        >
+                            Type here (e.g. 'sporting event')
+                        </CardSubtitle>
+                    </CardBody>
+                </Card>
+
+                <Card
+                    style={{
+                        width: '18rem'
+                    }}
+                >
+                    <CardImg
+                        alt='generic decorative image'
+                        src='assets/seahawks.png'
+                        width="100%"
+                    />
+
+                    <CardBody>
+                        <CardTitle tag="h5">
+                            name (e.g. Seahawks v. Someone at Place)
+                        </CardTitle>
+                        <CardSubtitle
+                            className="mb-2 text-muted"
+                            tag="h6"
+                        >
+                            Type here (e.g. 'sporting event')
+                        </CardSubtitle>
+                    </CardBody>
+                </Card>
+
+                <Card
+                    style={{
+                        width: '18rem'
+                    }}
+                >
+                    <CardImg
+                        alt='generic decorative image'
+                        src='assets/seahawks.png'
+                        width="100%"
+                    />
+
+                    <CardBody>
+                        <CardTitle tag="h5">
+                            name (e.g. Seahawks v. Someone at Place)
+                        </CardTitle>
+                        <CardSubtitle
+                            className="mb-2 text-muted"
+                            tag="h6"
+                        >
+                            Type here (e.g. 'sporting event')
+                        </CardSubtitle>
+                    </CardBody>
+                </Card>
+
+                <Card
+                    style={{
+                        width: '18rem'
+                    }}
+                >
+                    <CardImg
+                        alt='generic decorative image'
+                        src='assets/seahawks.png'
+                        width="100%"
+                    />
+
+                    <CardBody>
+                        <CardTitle tag="h5">
+                            name (e.g. Seahawks v. Someone at Place)
+                        </CardTitle>
+                        <CardSubtitle
+                            className="mb-2 text-muted"
+                            tag="h6"
+                        >
+                            Type here (e.g. 'sporting event')
+                        </CardSubtitle>
+                    </CardBody>
+                </Card>
+
+                <Card
+                    style={{
+                        width: '18rem'
+                    }}
+                >
+                    <CardImg
+                        alt='generic decorative image'
+                        src='assets/seahawks.png'
+                        width="100%"
+                    />
+
+                    <CardBody>
+                        <CardTitle tag="h5">
+                            name (e.g. Seahawks v. Someone at Place)
+                        </CardTitle>
+                        <CardSubtitle
+                            className="mb-2 text-muted"
+                            tag="h6"
+                        >
+                            Type here (e.g. 'sporting event')
+                        </CardSubtitle>
+                    </CardBody>
+                </Card>
+
+            </CardGroup>
+
+            
+
+        </div>
+
+
+    )
+}
+
+
