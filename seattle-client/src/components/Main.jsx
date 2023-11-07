0a1bcdb808588f18556325b87f1c9697a0a32980
Rhys Frazier <rhysrfrazier@gmail.com>
2023-11-07 09:54:25 -0500

    crying

--- seattle-client/src/components/Main.jsx
+++ seattle-client/src/components/Main.jsx
@@ -20,7 +20,7 @@ export default function Main() {
                 <Route path='/RestaurantList' element={<RestaurantList />} />
                 <Route path='/RestaurantPage' element={<RestaurantPage />} /> */}
                 <Route path='/ActivityList' element={<ActivityList />} />
-                <Route path='/ActivityPage' element={<ActivityPage />} /> */}
+                <Route path='/ActivityPage' element={<ActivityPage />} /> 
                 <Route path='/HotelList' element={<HotelList />} />
                 <Route path='/HotelList/:id' element={<HotelPage />} />
                 {/* <Route path='/DistrictList' element={<DistrictList />} /> */}
