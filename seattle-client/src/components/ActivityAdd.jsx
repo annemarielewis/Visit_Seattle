365d4807917ec38f6f9d40f4c3feb24e16b15fa0
annemarielewis <a.m.lewisfilms@gmail.com>
2023-11-09 10:21:21 -0700

    update

--- seattle-client/src/components/ActivityAdd.jsx
+++ seattle-client/src/components/ActivityAdd.jsx
@@ -17,8 +17,13 @@ export default function addActivity() {
 
   const [formState, setFormState] = useState(initialState);
 
+  const [deleteState, setDeleteState] = useState(initialState);
+
   const handleChange = (event) =>
     setFormState({ ...formState, [event.target.id]: event.target.value });
+
+  const handleChangeDelete = (event) =>
+    setDeleteState({ ...deleteState, [event.target.id]: event.target.value });
 
   console.log(formState);
 
@@ -59,102 +64,138 @@ export default function addActivity() {
     }
   };
 
+  const handleDelete = async (event) => {
+    event.preventDefault();
+    console.log(deleteState);
+    const dataIDToDelete = deleteState.id;
+    console.log(dataIDToDelete);
+    try {
+      axios.delete(`http://localhost:3001/deleteactivity/${dataIDToDelete}`);
+    } catch (error) {
+      console.error("An error occurred:", error);
+    }
+  };
+
   return (
     <>
-      <form className="form" onSubmit={handleSubmit}>
-        <label htmlFor="name">Activity:</label>
-        <input
-          name=""
-          id="name"
-          cols="30"
-          rows="1"
-          onChange={handleChange}
-          value={formState.activity}
-        />
+      {/* ADD */}
+      <div className="add">
+        <h1>Add an Activity</h1>
+        <form className="grid" onSubmit={handleSubmit}>
+          <label htmlFor="name">Activity:</label>
+          <input
+            name=""
+            id="name"
+            cols="30"
+            rows="1"
+            onChange={handleChange}
+            value={formState.activity}
+          />
 
-        <label htmlFor="type">Type:</label>
-        <input
-          name=""
-          id="type"
-          cols="30"
-          rows="2"
-          onChange={handleChange}
-          value={formState.type}
-        />
-        <label htmlFor="number">Number:</label>
-        <input
-          name=""
-          id="number"
-          cols="30"
-          rows="2"
-          onChange={handleChange}
-          value={formState.number}
-        />
-        <label htmlFor="address">Address:</label>
-        <input
-          name=""
-          id="address"
-          cols="30"
-          rows="2"
-          onChange={handleChange}
-          value={formState.address}
-        />
-        <label htmlFor="url">URL:</label>
-        <input
-          name=""
-          id="url"
-          cols="30"
-          rows="2"
-          onChange={handleChange}
-          value={formState.url}
-        />
-        <label htmlFor="priceRating">Price Rating:</label>
-        <input
-          name=""
-          id="priceRating"
-          cols="30"
-          rows="2"
-          onChange={handleChange}
-          value={formState.priceRating}
-        />
-        <label htmlFor="familyFriendly">Family Friendly:</label>
-        <input
-          name=""
-          id="familyFriendly"
-          cols="30"
-          rows="2"
-          onChange={handleChange}
-          value={formState.familyFriendly}
-        />
-        <label htmlFor="district">District:</label>
-        <input
-          name=""
-          id="district"
-          cols="30"
-          rows="2"
-          onChange={handleChange}
-          value={formState.district}
-        />
-        <label htmlFor="image">Image:</label>
-        <input
-          name=""
-          id="image"
-          cols="30"
-          rows="2"
-          onChange={handleChange}
-          value={formState.image}
-        />
-        <label htmlFor="description">Description:</label>
-        <input
-          name=""
-          id="description"
-          cols="30"
-          rows="2"
-          onChange={handleChange}
-          value={formState.description}
-        />
-        <button type="submit">Send</button>
-      </form>
+          <label htmlFor="type">Type:</label>
+          <input
+            name=""
+            id="type"
+            cols="30"
+            rows="2"
+            onChange={handleChange}
+            value={formState.type}
+          />
+          <label htmlFor="number">Number:</label>
+          <input
+            name=""
+            id="number"
+            cols="30"
+            rows="2"
+            onChange={handleChange}
+            value={formState.number}
+          />
+          <label htmlFor="address">Address:</label>
+          <input
+            name=""
+            id="address"
+            cols="30"
+            rows="2"
+            onChange={handleChange}
+            value={formState.address}
+          />
+          <label htmlFor="url">URL:</label>
+          <input
+            name=""
+            id="url"
+            cols="30"
+            rows="2"
+            onChange={handleChange}
+            value={formState.url}
+          />
+          <label htmlFor="priceRating">Price Rating:</label>
+          <input
+            name=""
+            id="priceRating"
+            cols="30"
+            rows="2"
+            onChange={handleChange}
+            value={formState.priceRating}
+          />
+          <label htmlFor="familyFriendly">Family Friendly:</label>
+          <input
+            name=""
+            id="familyFriendly"
+            cols="30"
+            rows="2"
+            onChange={handleChange}
+            value={formState.familyFriendly}
+          />
+          <label htmlFor="district">District:</label>
+          <input
+            name=""
+            id="district"
+            cols="30"
+            rows="2"
+            onChange={handleChange}
+            value={formState.district}
+          />
+          <label htmlFor="image">Image:</label>
+          <input
+            name=""
+            id="image"
+            cols="30"
+            rows="2"
+            onChange={handleChange}
+            value={formState.image}
+          />
+          <label htmlFor="description">Description:</label>
+          <input
+            name=""
+            id="description"
+            cols="30"
+            rows="2"
+            onChange={handleChange}
+            value={formState.description}
+          />
+          <button type="submit">Send</button>
+        </form>
+
+        {/* DELETE */}
+        <div className="delete">
+          <h1>Delete an Activity</h1>
+          <form className="grid" onSubmit={handleDelete}>
+            <label htmlFor="id">Activity ID:</label>
+            <input
+              type="text"
+              id="id"
+              name=""
+              onChange={handleChangeDelete}
+              value={formState.id}
+              required
+            />
+            <br></br>
+            <button type="submit">Delete Activity</button>
+          </form>
+        </div>
+
+        {/* UPDATE */}
+      </div>
     </>
   );
 }
