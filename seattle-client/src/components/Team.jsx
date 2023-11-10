4452252ba7053810905ef38b57fe1f6094d9f2e2
annemarielewis <a.m.lewisfilms@gmail.com>
2023-11-09 19:58:58 -0700

    video added
    # Conflicts:
    #	seattle-client/src/components/Team.jsx

--- seattle-client/src/components/Team.jsx
+++ seattle-client/src/components/Team.jsx
@@ -1,13 +1,23 @@
 export default function Team() {
-    return (
-        <div className="Team">
-            <h4>Project Team Members:</h4>
-            <h5><span>Annemarie Lewis</span> <a href="https://github.com/annemarielewis"></a></h5>
-            <h5><span>Haroon Frotan</span> <a href="https://github.com/HaroonFro"></a></h5>
-            <h5><span>Rhys Frazier</span> <a href="https://github.com/rhysrfrazier"></a></h5>
-            <h5><span>Ryan Fitzgerald</span> <a href="https://github.com/Ryan-Fitzgerald-9"></a></h5>
-        </div>
-    )
+  return (
+    <div className="Team">
+      <h4>Project Team Members</h4>
+      <h5>
+        <span>Annemarie Lewis</span>{" "}
+        <a href="https://github.com/annemarielewis"></a>
+      </h5>
+      <h5>
+        <span>Haroon Frotan</span> <a href="https://github.com/HaroonFro"></a>
+      </h5>
+      <h5>
+        <span>Rhys Frazier</span> <a href="https://github.com/rhysrfrazier"></a>
+      </h5>
+      <h5>
+        <span>Ryan Fitzgerald</span>{" "}
+        <a href="https://github.com/Ryan-Fitzgerald-9"></a>
+      </h5>
+    </div>
+  );
 }
 
 {/* <h4>Project Team Members:</h4>
