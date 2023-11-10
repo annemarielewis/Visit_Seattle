4eed7cb22c6409b38d5f1ef51dc289413530d506
Rhys Frazier <rhysrfrazier@gmail.com>
2023-11-10 13:49:31 -0500

    fixed boolean nonsense

--- seattle-client/src/components/ActivityPage.jsx
+++ seattle-client/src/components/ActivityPage.jsx
@@ -2,6 +2,15 @@ import React from 'react'
 import './Overlay.css'
 
 export default function ActivityPage({ activity, onClose }) {
+
+    let familyFriendly
+
+    if (activity.familyFriendly === true) {
+        familyFriendly = 'yes'
+    } else {
+        familyFriendly = 'no'
+    }
+
       return (
           <div className="card-details-overlay">
               <div className="card-details-content">
@@ -12,7 +21,7 @@ export default function ActivityPage({ activity, onClose }) {
                       <img src={activity.image} alt={activity.name} />
                       <h3>{activity.name}</h3>
                       <p>Type: {activity.type}</p>
-                      <p>Family Friendly: {activity.familyFriendly}</p>
+                      <p>Family Friendly: {familyFriendly}</p>
                       <a href={activity.url}>Visit {activity.name}</a>
                       <p>{activity.description}</p>
                   </div>
